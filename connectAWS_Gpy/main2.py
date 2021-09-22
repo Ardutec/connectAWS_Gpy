4ea1427f1708168b354dcafde95efe6495e2dc80
Ardutec <34646434+Ardutec@users.noreply.github.com>
2021-09-22 15:23:13 +0500

    Update main2.py

--- connectAWS_Gpy/main2.py
+++ connectAWS_Gpy/main2.py
@@ -1,24 +1,29 @@
-from mqtt import MQTTClient
+import mqtt
 from network import WLAN
 import machine
 import time
-import binascii
 import abdTest
 
-#AWS_ROOT_CA = '/flash/cert/AmazonRootCA3.pem'
-AWS_CLIENT_CERT = '/flash/cert/certificate.pem.crt'
-AWS_PRIVATE_KEY = '/flash/cert/private.pem.key'
-
-def sub_cb(topic, msg):
-   print(msg)
 prKeyAddress = 28418
 pbKeyAddress = 28419
 cerAddress = 28417
-wifi_ssid = "YOUR_NETWORK_SSID"
-wifi_pass = "YOUR_NETWORK_PASSWORD"
+
+wifi_ssid = "SSID"
+wifi_pass = "PASSWORD"
+
+abdTest.readCer(cerAddress)
+time.sleep(1)
+abdTest.readCer(prKeyAddress)
+time.sleep(1)
+AWS_CLIENT_CERT = '/flash/cert/certificateMS.pem.crt'
+#AWS_ROOT_CA = '/flash/cert/AmazonRootCA3MS.pem'
+AWS_PRIVATE_KEY = '/flash/cert/privateMS.pem.key'
+
+def sub_cb(topic, msg):
+   print(msg)
 
 wlan = WLAN(mode=WLAN.STA)
-wlan.connect(wifi_ssid, auth=(WLAN.WPA2, wifi_pass), timeout=5000)
+wlan.connect(wifi_ssid, auth=(WLAN.WPA2, wifi_pass), timeout=10000)
 while not wlan.isconnected():
     machine.idle()
 print("Connected to WiFi\n")
@@ -29,18 +34,11 @@ clientID = "PYCOM_GPY_3"
 topicPb = "$aws/things/tealcomtest1/shadow/update"
 topicSb= "$aws/things/tealcomtest1/shadow/update/accepted"
 topic = "thing/RaspberryPi1"
-#caCer = abdTest.readCer(cerAddress)
-#prKey = abdTest.readCer(prKeyAddress)
-#print(binascii.unhexlify(prKey))
-#pbKey = abdTest.readCer(pbKeyAddress)
 
-#                    "a2ezy2sb17mjr3-ats.iot.eu-central-1.amazonaws.com",
-#                    "cert":binascii.unhexlify(caCer),
-#                    "key":binascii.unhexlify(prKey),
-#                    "server_side":False
-client = MQTTClient(clientID,
-                    AWS_URL,
-                    port=AWS_Port,
+
+client = mqtt.MQTTClient("PYCOMGPY3",
+                    "a2ezy2sb17mjr3-ats.iot.eu-central-1.amazonaws.com",
+                    port=8883,
                     keepalive=10000,
                     ssl=True,
                     ssl_params={
@@ -51,11 +49,13 @@ client = MQTTClient(clientID,
                     })
 time.sleep(2)
 client.set_callback(sub_cb)
+
+#client.subscribe(topic=topic)
 client.connect()
-client.subscribe(topic=topic)
+client.subscribe(topic=topicSb)
 while True:
     print("Sending ON")
-    client.publish(topic=topic, msg='{"state":{"reported":{"msg": "hello world from Pycom Board"}}}', qos = 0)
+    client.publish(topic=topicPb, msg='{"state":{"reported":{"msg": "hello world from Pycom Board"}}}', qos = 0)
     print("Sent")
     time.sleep(5)
     client.check_msg()
